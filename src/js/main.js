800e731e5cc6764e18cbed92c77808ff52478ecc
Sebastian <sebastian.malicki02@gmail.com>
2022-05-31 15:40:11 +0200

    improved cookie(counter) performance and added reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,53 @@
 let buttons = document.querySelectorAll(".button");
-let counter = 0;
-document.cookie = "counter =" + 0;
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
+//function that get value of cookie counter
+function getCookieValue (name) {
+	let value = `; ${document.cookie}`;
+	let parts = value.split(`; ${name}=`);
+    // console.log(value,parts);
+	if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    } else {}
+}
+
+//function that reset counter to zero 
+function resetCounter() {
+    document.cookie = "counter=" + 0;
+    document.getElementById("popup-content").innerHTML = "You have clicked <strong>" + 0 + " times </strong> to related button.";
+}
+
+window.onload = () => {
+    
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            let counter = parseInt(getCookieValue('counter'));
+            
+            if (counter >= 5) {
+                document.querySelector("#reset-button").classList.remove("inactive");
+                document.querySelector("#reset-button").classList.add("active");
+            } else {
+                document.querySelector("#reset-button").classList.add("inactive");
+                document.querySelector("#reset-button").classList.remove("active");
+            }
+
+            counter += 1;
+            // console.log(counter);
         
-        counter += 1;
-        console.log(counter);
-        // alert("dupa");
-        document.querySelector("#popup-window").classList.add("active");
-        document.querySelector("#popup-window").classList.remove("inactive");
-        document.querySelector("#overlay").classList.add("active");
-        document.querySelector("#overlay").classList.remove("inactive");
-        document.getElementById("popup-content").innerText = `You have clicked ${counter} times to related button.`;
+            document.querySelector("#popup-window").classList.add("active");
+            document.querySelector("#popup-window").classList.remove("inactive");
+            document.querySelector("#overlay").classList.add("active");
+            document.querySelector("#overlay").classList.remove("inactive");
+            document.getElementById("popup-content").innerHTML = "You have clicked <strong>" + `${counter}` + " times </strong> to related button.";
 
-        document.cookie = "counter ="+ counter;
-        console.log(document.cookie);
-        // or use return fuction to return popup-content id with counter inside
+            document.cookie = "counter=" + counter;
+            // console.log(document.cookie);
+        });
     });
-});
+}
 
 // function that close popup window
 function closePopupWindow() {
-    // document.querySelector('.popup-window').remove();
+    // document.querySelector('#popup-window').remove();
     document.querySelector("#popup-window").classList.remove("active");
     document.querySelector("#popup-window").classList.add("inactive");
     document.querySelector("#overlay").classList.remove("active");
